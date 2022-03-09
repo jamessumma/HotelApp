23db4b9683c13f9279062155e65c94047ebbcc64
james <jsumma3167@westfield.ma.edu>
2022-03-09 16:34:25 -0500

    added some login stuff

---
+++ app/src/main/java/com/example/hotelapp/Activities/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.hotelapp.Activities;
+import com.example.myandroidsupportlibrary.DatabaseSupport.DatabaseAccess.DatabaseController;
+import com.example.myandroidsupportlibrary.DatabaseSupport.DatabaseAccess.DatabaseLogin.LoginValidation;
+public class LoginActivity {
+
+}

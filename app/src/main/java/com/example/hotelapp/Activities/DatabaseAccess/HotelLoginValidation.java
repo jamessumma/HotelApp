3dee611b31d9c48f25c1637448f63d388347258f
james <jsumma3167@westfield.ma.edu>
2022-03-09 16:49:46 -0500

    added more login stuff

---
+++ app/src/main/java/com/example/hotelapp/Activities/DatabaseAccess/HotelLoginValidation.java
@@ -0,0 +1,41 @@
+package com.example.hotelapp.Activities.DatabaseAccess;
+
+import com.example.myandroidsupportlibrary.DatabaseSupport.DatabaseAccess.DatabaseController;
+import com.example.myandroidsupportlibrary.DatabaseSupport.DatabaseAccess.DatabaseLogin.LoginValidation;
+import android.content.Context;
+import android.content.Intent;
+
+public class HotelLoginValidation extends LoginValidation{
+
+    public HotelLoginValidation(DatabaseController databaseController,
+                                        String username,
+                                        String password)
+    {
+        super(databaseController,username,password);
+    }
+
+    @Override
+    public String getUsernameField() {
+        return "username";
+    }
+
+    @Override
+    public String getPasswordField() {
+        return "password";
+    }
+
+    @Override
+    public String getLoginTable() {
+        return "LoginInfo";
+    }
+
+    @Override
+    public void onLoginSuccess(Context context) {
+        //add home screen here
+    }
+
+    @Override
+    public void onLoginFail(Context context) {
+        // add toast here
+    }
+}
